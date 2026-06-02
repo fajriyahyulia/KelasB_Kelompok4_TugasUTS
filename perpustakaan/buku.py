0d00b6bbc84c11f661f52e410edfd0e6c89349ba
riskarahmaa11 <riskarahmaa4@gmail.com>
2026-06-02 09:29:39 +0700

    Update buku.py

--- perpustakaan/buku.py
+++ perpustakaan/buku.py
@@ -1,2 +1,14 @@
-# dikerjakan oeh: Rahma
+# dikerjakan oeh: Riska Nur Rahmawati (K3525039)
 # username: (riskarahmaa11)
+
+from perpustakaan.koleksi import Koleksi
+
+class Buku(Koleksi):
+    def __init__(self, kode, judul, tahun, pengarang, penerbit):
+        super().__init__(kode, judul, tahun, penerbit)
+        self.pengarang = pengarang
+    
+    def tampilkan(self):
+        print("Jenis    : Buku")
+        self.tampilkan_dasar()
+        print(f"Pengarang: {self.pengarang}")
