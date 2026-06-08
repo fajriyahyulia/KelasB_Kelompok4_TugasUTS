bee6e1dc75817c75912670519dab953170e8ecbe
Vincensius Vicko <pikoopik27@student.uns.ac.id>
2026-06-08 22:25:41 +0700

    Update majalah.py

--- perpustakaan/majalah.py
+++ perpustakaan/majalah.py
@@ -1,14 +1,17 @@
 # dikerjakan oleh: Vincensius Vicko R.S (K3525042)
-# username: (pikoopikk)
-
+# username: pikoopikk
 from perpustakaan.koleksi import Koleksi
 
 class Majalah(Koleksi):
     def __init__(self, kode, judul, tahun, penerbit, edisi):
         super().__init__(kode, judul, tahun, penerbit)
         self.edisi = edisi
-    
+
+    def get_jenis(self):
+        return "Majalah"
+
     def tampilkan(self):
+        print("=" * 40)
         print("Jenis    : Majalah")
         self.tampilkan_dasar()
         print(f"Edisi    : {self.edisi}")
